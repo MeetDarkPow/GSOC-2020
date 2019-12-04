55bba470470d8a8bb0f0c3fb235b310e92e060c9
Meet Bhatnagar <46255664+MeetDarkPow@users.noreply.github.com>
2019-12-04 19:35:01 +0530

    Update Test_1.R

--- Test_1.R
+++ Test_1.R
@@ -9,50 +9,43 @@ library(ggplot2)
 
 # Taking input to create user friendly data set 
 
-S <- as.integer(readline(prompt="Enter size of Data set: "))
-N <- as.integer(readline(prompt="Enter number of Data elements in each set: "))
-
-# Data frame for rpois() values
-
-df <- data.frame(matrix(ncol = S, nrow = N))
-creating_dataset <- function(N, S) {
-  for (i in 1:S) {
-    df[, i] <<- rpois(N,50) # Assuming 50 as vector of (non-negative) means
-  }
-}
+N <- as.integer(readline(prompt="Enter size of Data set: "))
+test_size_value <- c(N, 10*N, 100*N, 1000*N)
 
 # Two empty variables to hold computation time
 
-cdpa <- c()
-pdpa <- c()
+cdpa <- integer(length(test_size_value))
+pdpa <- integer(length(test_size_value))
 
-# Function to calculate computation time of cDPA() and PeakSegPDPA()
+# Function to calculate computation time of cDPA() and PeakSegPDPA() Function
+# computation_time function 2 parameters.
+# First  -> ms which corresponds to number of max segements
+# Second -> lambda which corresponds to vector of means
 
-computation_time <- function(N, S) {
+computation_time <- function(ms, lambda) {
   # for() loop used to compute each set computation time
-  for (i in 1:S) {
-    value_cdpa <- cDPA(df[, i], maxSegments = 2L)
-    value_pdpa <- PeakSegPDPA(df[, i], max.segments = 2L)
-    mbm <- microbenchmark(value_cdpa, value_pdpa, times = 1000L)
-    execution_time <- summary(mbm)$mean      # Extracting computation time
-    cdpa <<- append(cdpa, execution_time[1]) # Storing extracted computation time 
-    pdpa <<- append(pdpa, execution_time[2]) # Storing extracted computation time 
+  for (i in 1:length(test_size_value)) {
+    mbm <- microbenchmark(cDPA(rpois(test_size_value[i], lambda), maxSegments = as.integer(ms)),
+                          PeakSegPDPA(rpois(test_size_value[i], lambda), max.segments = as.integer(ms)))
+    execution_time <- summary(mbm)$mean   # Extracting computation time
+    cdpa[i] <<- execution_time[1]         # Storing extracted computation time 
+    pdpa[i] <<- execution_time[2]         # Storing extracted computation time 
   }
 }
 
 # Function for plotting graph of PeakSegDP::cDPA versus PeakSegOptimal::PeakSegPDPA
+# computation_graph has 1 parameter
+# First -> sets which corresponds to number of data sets we want to display
 
-computation_graph <- function(S) {
-coord <- data.frame(cdpa, pdpa)    # Data frame for co-ordinates of cdpa and pdpa
-data_set_num <- c(1:S)
-ggplot(coord, aes(data_set_num)) + 
-  geom_line(aes(y = cdpa, colour = "cDPA")) + 
-  geom_line(aes(y = pdpa, colour = "PeakSegPDPA")) +
-  ggtitle("PeakSegDP::cDPA versus PeakSegOptimal::PeakSegPDPA") +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  scale_x_log10(breaks = seq(0, S, by = 5)) + 
-  scale_y_log10(breaks = seq(0, 1000, by = 2)) + 
-  xlab("Data Set Number") + labs(colour = "Function Comparison") + 
-  ylab("Execution Time (in Nanoseconds)") + 
-  theme_bw(base_size = 15)
+computation_graph <- function(sets) {
+  coord <- data.frame(cdpa, pdpa, test_size_value)  
+  ggplot(coord, aes(test_size_value)) + 
+    geom_line(aes(y = cdpa, colour = "cDPA")) + 
+    geom_line(aes(y = pdpa, colour = "PeakSegPDPA")) +
+    ggtitle("PeakSegDP::cDPA versus PeakSegOptimal::PeakSegPDPA") +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    scale_x_log10() + scale_y_log10() +
+    xlab("N") + labs(colour = "Function Comparison") + 
+    ylab("Execution Time") + 
+    theme_bw(base_size = 15)
 }
